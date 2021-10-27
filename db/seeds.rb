c64fd32f0b06b36c245daf27709c4a040079ee5e
Simon Boutin <simboutin@gmail.com>
2021-10-27 16:47:43 +0200

    scaffold gardens

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database'
+Garden.destroy_all
+puts 'Database has been cleaned'
+
+puts 'Creating gardens'
+Garden.create!(
+  name: "Jardin d'Eden",
+  banner_url: 'https://images.unsplash.com/photo-1560183721-b1a4a7768bbd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80'
+)
+
+Garden.create!(
+  name: "Mon potager",
+  banner_url: 'https://images.unsplash.com/photo-1592424451528-a5a6ae06af1c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80s'
+)
+
+puts 'All gardens have been created!'
